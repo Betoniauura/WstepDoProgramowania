d1b4bc72d966d3d04ffbaf3c100389e78ddb79b7
Betoniauura <kubajurzysta@o2.pl>
2022-10-27 12:44:19 +0200

    Tu zaczynam zabawe z lab3

---
+++ LAB3najwazniejsze/Lab3niestety.py
@@ -0,0 +1,1 @@
+Wargson asdasdasd
--- LAB3najwazniejsze/main.py
+++ LAB3najwazniejsze/main.py
@@ -1,1 +1,11 @@
-coś tu będzie
+x = -4
+while x <= 4:
+    if -2 <= x <= 2:
+        x = x + 0.5
+        continue
+    y = 2*x**2 -5*x -8
+    print(f"f({x}) = {y}")
+    x = x + 0.5
+
+print("koniec", x)
+
