9ea3ee0898b86b50e84c50be0da99b01e97ab5be
Betoniauura <kubajurzysta@o2.pl>
2022-11-03 13:27:10 +0100

    Lab continue

--- pythonProject/LabContinueITD/main.py
+++ pythonProject/LabContinueITD/main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+'''
+n = int(input("Proszę podać liczbę studentów: "))
+a = 1 #wyświetlanie od 1 studenta
+c = 0
+ #podstawienie pod dodawanie punktów
+while True:
+    b = int(input(f"Proszę podać punkty studenta {a}: "))
+    if b<0 or b>100:
+        continue
+        c += b
+        a += 1
+        if a == n+1:
+            break
+y = c/n
+print("Średnia wszystkich studentów",y)
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+for i in range(100,-1,-10):
+    print(i)
 
+b = int(input("Podaj wyskokośćS:"))
+for i in range(b):
+    for i in range(b):
+          print("*",end="")
+print()
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+x = ["Jajka","Mleko","Śmietana","Mąka","CocaCola"]
 
+print(x)
+print(x[0],x[1])
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+x[2]="mleko"
+print(x)
+print(x[-1],x[-2])
+'''
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+x = []
+a = int(input("Podaj ilość elementów listy"))
+import random
+k = random.randint(1,10)
+
+x.append(k)
+print(x)
